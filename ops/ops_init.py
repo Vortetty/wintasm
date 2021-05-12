eb0de64ceb25b661aaad1b0566b478ede7c40712
vortetty <vortetty@gmail.com>
2021-05-12 16:00:33 -0700

    e

--- ops/ops_init.py
+++ ops/ops_init.py
@@ -10,14 +10,15 @@ import imp
 import os
 
 def init(ops: Dict[str, Callable[[List[int], int, lineCounter, str, List[str]], None]], ENABLE_HARD_MODE):
-    op_nand.init(ops)
-    op_jmpif.init(ops)
+    op_nand.init(ops)                                                            # The bare minimum modules
+    op_jmpif.init(ops)                                                           # The bare minimum modules
 
-    if not ENABLE_HARD_MODE:
-        directory = "ops/op_src"
+    if not ENABLE_HARD_MODE:                                                     # Dont enable everything unless hard mode is off
+        directory = "ops/op_src"                                                 # Directory that modules are stored in
 
-        list_modules=os.listdir(directory)
-        for module_name in list_modules:
-            if module_name.split('.')[-1] == 'py':
-                module = imp.load_source('module', directory+os.sep+module_name)
-                module.init(ops)
+        list_modules=os.listdir(directory)                                       # Get a list of available modules
+        for module_name in list_modules:                                         # Iterate modules
+            if module_name.split('.')[-1] == 'py':                               # Check that file is a python file
+                module = imp.load_source('module', directory+os.sep+module_name) # Load module 
+                func = getattr(module, module_name[:-3])                         # Get the function (it should have the same name as the file)
+                ops[module_name[3:-3]] = func                                    # Set the function to the proper name
