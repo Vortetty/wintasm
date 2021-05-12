1bbced0e4139c851c18902c7f65ea03516bd7967
vortetty <vortetty@gmail.com>
2021-05-12 15:12:35 -0700

    e

--- ops/ops_init.py
+++ ops/ops_init.py
@@ -2,21 +2,22 @@ from typing import List, Dict, Callable
 from globs import *
 
 # Hard mode
-from ops import op_nand
-from ops import op_jmpif
+from ops.op_src import op_nand
+from ops.op_src import op_jmpif
 
 # Normal mode
-from ops import op_set
-from ops import op_xor
-from ops import op_and
-from ops import op_not
+import imp
+import os
 
 def init(ops: Dict[str, Callable[[List[int], int, lineCounter, str, List[str]], None]], ENABLE_HARD_MODE):
     op_nand.init(ops)
     op_jmpif.init(ops)
 
     if not ENABLE_HARD_MODE:
-        op_set.init(ops)
-        op_xor.init(ops)
-        op_and.init(ops)
-        op_not.init(ops)
+        directory = "ops/op_src"
+
+        list_modules=os.listdir(directory)
+        for module_name in list_modules:
+            if module_name.split('.')[-1] == 'py':
+                module = imp.load_source('module', directory+os.sep+module_name)
+                module.init(ops)
